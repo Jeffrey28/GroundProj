9a746dffa18e8b8ea6ab6c84e1ed34b1132d7e8d
Daryl Bennett <kd8bny@gmail.com>
2013-11-09 14:58:06 -0500

    receiver and error checking

--- lms/sMN_setAccessMode.m
+++ lms/sMN_setAccessMode.m
@@ -2,11 +2,9 @@
 %Use of SICK LMS 111 
 %Daryl W. Bennett ~dwbennet@mtu.edu ~kd8bny@gmail.com
 %Purpose: Function to Set Access Mode
-%TODO: Make function
 
-%Notes: All binary (Could use HEX in future)
 %Command Structure:
-%[Start text][MSG length][CMD Type][SPC][CMD][SPC][USRlvl][PSWRD][CHKSUM]
+%[STX][CMD Type][SPC][CMD][SPC][USRlvl][PSWRD][ETX]
 
 %% Alt Values
 %USRlvl: [02:Maintenance; 03:Auth Client; 04:Service]
@@ -14,56 +12,31 @@
 
 %% Setup
 clc,clear
-%Delclare telegram. See: INFO/command structure
-telegramCell = {};
-STX = {'02','02','02','02'};
-SPC = {'20'};
-
-%LMS requires user login (USRlvl & PSWRD)
-%Predefined: See INFO/Command Structure
-CMDtype = {'73','4D','4E'};
-CMD = {'53','65','74','41','63','63','65','73','73','4D','6F','64','65'};
-
-%Value for Authorized Client. See /Alt Values
-USRlvl = {'03'};
-PSWRD = {'F4','72','47','44'};
-
-%% Set Telegam
-% See: INFO/command structure
-telegramCell(1:4) = STX(1:4);
-%compute telegramCell(5:8) last for modular code
-telegramCell(9:11) = CMDtype(1:3);
-telegramCell(12) = SPC(1);
-telegramCell(13:25) = CMD(1:13);
-telegramCell(26) = SPC(1);
-telegramCell(27) = USRlvl(1);
-telegramCell(28:31) = PSWRD(1:4);
-telegramCell(32) = {CHKSUM(telegramCell(9:31))};  %Check Sum
-
-telegramCell(5:8) = findLength(length(telegramCell(9:31)));
-
+telegram ='sMN SetAccessMode 03 F4724744';
 %%now to send telegram
-sendTelegram(telegramCell)
+RXtelegram = sendTelegram(telegram);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 %% Receiver
-%code receive based on LIDAR output 
-%for now using cell array
-%Default:
-telegramRX = {'02','02','02','02','00','00','00','13','73','41','4E','20','53','65','74','41','63','63','65','73','73','4D','6F','64','65','20','01','39'};
+%code receive based on LIDAR output
+
+%Note: message is received as dec values of ascii char
+%Command Structure:
+%[STX][CMD Type][SPC][CMD][SPC][ERROR?][ETX]
 
 % Preset
-Error = {'00'};
-Success = {'01'};
-RX_L = length(telegramRX);
-MSGRX_L = hex2dec(telegramRX(8));    %Grabs last value only
+Error = 48; %0 ascii
+Success = 49; %1ascii
+RX_L = length(RXtelegram);
+
 %Grab "Change user level success"
-value = telegramRX(8+MSGRX_L);
-if(isequal(value,Error))
-    fprintf('Error in Change user level\n')
-else
+value = RXtelegram(RX_L-1); %Error value
+if(isequal(value,Success))
     fprintf('Success\n')
+else
+    fprintf('Error in Change user level\n')
 end
+pause(10)
 
 
 
