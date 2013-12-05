122c13afdf2142952e2f0150e815653cd643884a
Daryl Bennett <kd8bny@gmail.com>
2013-12-04 21:26:38 -0500

    Initial Commit. Read constant stream.

---
+++ lms/telegramStream.m
@@ -0,0 +1,18 @@
+%% INFO
+%Use of SICK LMS 111 
+%Daryl W. Bennett ~dwbennet@mtu.edu ~kd8bny@gmail.com
+%Purpose: Consistant stream of OUTPUT
+%TODO: Make function
+
+%Command Structure: [SPC] is implicated
+%[STX][CMD Type][Version][Device][Serial][Status][TCounter][SCounter][runTime][TXtime][Din Status][Dout Status][RES][ScanFreq]
+%	[Measured Freq][Enco Pos][Enco Speed][Amount data][Data_ch][Pos][X][Y][Z][Xangle][Yangle][Zangle][angleType][DeviceName]
+%		[LengthName][Name][Comment][LengthComment][comment][time][...][ETX]
+%% Alt Values
+%Read pg 31-33
+
+%% Read Stream
+RXtelegram = sendTelegram(telegram);
+%TODO: Timeout
+
+%realize Values
