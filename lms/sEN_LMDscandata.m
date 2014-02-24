5fc8a7b37d585b6aa6979bf5532c3b868594b76b
Daryl Bennett <kd8bny@gmail.com>
2014-02-24 14:01:23 -0500

    WIP

--- lms/sEN_LMDscandata.m
+++ lms/sEN_LMDscandata.m
@@ -8,32 +8,41 @@
 %[STX][CMD Type][SPC][CMD][SPC][Start/Finish][ETX]
 
 %% Alt Values
-%Start/Finish: [START: 1; Stop: 0]
+%Start/Finish: [Start: 1; Stop: 0]
 
 %% Setup
 clc,clear
 global log
-telegram ='sEN LMDscandata 1';
-%%now to send telegram
-RXtelegram = sendTelegram(telegram);
 
+fprintf(log,'%17s\n','Starting Data Log');
+telegram ='sEN LMDscandata 1';
+    
 %% Receiver
 %code receive based on LIDAR output
 
 %Note: message is received as dec values of ascii char
 %Command Structure:
-%[STX][CMD Type][SPC][CMD][SPC][Start/Finish][ETX]
+%[STX][CMD Type][Version][Device][Serial][Status][TCounter][SCounter][runTime][TXtime][Din Status][Dout Status][RES][ScanFreq]
+%	[Measured Freq][Enco Pos][Enco Speed][Amount data][Data_ch][Pos][X][Y][Z][Xangle][Yangle][Zangle][angleType][DeviceName]
+%		[LengthName][Name][Comment][LengthComment][comment][time][...][ETX]
+
+%while(1)
+    RXtelegram = sendTelegram(telegram);
+    %fprintf(log,'%151d\n',RXtelegram);
+    pause(10)
+%end
+fclose(log);
 
 % Preset
-Stop = 48; %0 ascii
-Start = 49; %1 ascii
-RX_L = length(RXtelegram);
-
-value = RXtelegram(RX_L-1);
-if(isequal(value,Start))
-    fprintf(log,'%14s \t %5s\n','sEN_LMDscandata','Start');
-else
-    fprintf(log,'%14s \t %5s\n','sEN_LMDscandata','Stop');
-end
+% Stop = 48; %0 ascii
+% Start = 49; %1 ascii
+% RX_L = length(RXtelegram);
+% 
+% value = RXtelegram(RX_L-1);
+% if(isequal(value,Start))
+%     fprintf(log,'%14s \t %5s\n','sEN_LMDscandata','Start');
+% else
+%     fprintf(log,'%14s \t %5s\n','sEN_LMDscandata','Stop');
+% end
 
 %Will now return stream
