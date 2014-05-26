160bde9c5f519c5a61c09622f48076034ae21ee6
Daryl Bennett <kd8bny@gmail.com>
2014-05-26 11:25:58 -0400

    Loop to entire size

--- lms/sendTelegram.m
+++ lms/sendTelegram.m
@@ -1,17 +1,30 @@
 %% INFO
-%Use of SICK LMS 111 
+%Use of SICK LMS 111
 %Daryl W. Bennett ~dwbennet@mtu.edu ~kd8bny@gmail.com
 %Purpose: Function to send msg to LMS 1xx
 
 %% NOTES
 % Set IP and Port in front end
-%% Function Header
-function RXT = sendTelegram (TXT)
-global address
 
+%% Function Header
+function [RXT,ETX,dataStart] = sendTelegram (TXT)
+global address
 fwrite(address,02); %STX
 fwrite(address,TXT);
 fwrite(address,03); %ETX
-RXT = fread(address); 
-%should be: RXT = fread(address,size,'*char');...... but matlab is dumb
+output=zeros(300,1);
+RXT=0;
+i=0;
+dataStart=0; %Where data start within telegram
+while RXT~=3
+    i=i+1;
+    [RXT, ETX] = fread(address,1);
+    output(i) = RXT;
+    if output(i)==32&&output(i-1)==49&&output(i-2)==65 %Catch data size "0hA1" in this case
+        if dataStart==0
+            dataStart=i;
+        end
+    end
+end
+RXT = output(1:i);
 end
